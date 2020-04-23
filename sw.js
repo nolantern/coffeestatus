487a395126c77f48c4a45837f7c74888d4ef93c0
Felix Kraemer <f.kraemer@posteo.de>
2020-04-23 16:36:56 +0200

    optimized font size in nav bar

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const cacheDynSize = -1;
 /*
 To update service worker and refreshe cache increase ...CacheName version
 */
-const staticCacheName = 'site-static-v9';
+const staticCacheName = 'site-static-v10';
 const dynamicCacheName = 'site-dynamic-v8';
 
 // static cached assets
