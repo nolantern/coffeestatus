4a4543ba5a342ef911674a5cf407be12b36961c7
nolantern <f.kraemer@posteo.de>
2020-04-25 14:48:40 +0200

    disabled google translator popup

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const cacheDynSize = -1;
 /*
 To update service worker and refreshe cache increase ...CacheName version
 */
-const staticCacheName = 'site-static-v14';
+const staticCacheName = 'site-static-v15';
 const dynamicCacheName = 'site-dynamic-v10';
 
 // static cached assets
